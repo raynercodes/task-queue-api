382faa60d9b54afc19f0f0a82222e640e990ac43
Rayner <raynercodes@gmail.com>
2026-04-20 20:11:17 -0400

    Added gunicorn in requirements.txt

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask, request
 from routes.task_routes import tasks_bp
 from routes.auth_routes import auth_bp
 from utils.responses import error_response
+import os
 
 app = Flask(__name__)
 
@@ -17,4 +18,5 @@ def handle_internal_error(e):
     return error_response(str(e), status=500)
 
 if __name__ == "__main__":
+    port = int(os.getenv("PORT", 5000))
     app.run(host="0.0.0.0", port=5000, debug=True)
